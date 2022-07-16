47e93989d97e821749f77275da1de0e307299dd7
Primitive J <hokecj@gmail.com>
2022-07-16 14:03:35 -0400

    typedefs

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,6 +1,6 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql`
+const typedefs = gql`
   type User {
     _id: ID
     username: String!
@@ -40,5 +40,5 @@ const typeDefs = gql`
     removeBook(bookId: ID!): User
   }
 `;
-//export typeDefs
-module.exports = typeDefs;
+//export typedefs
+module.exports = typedefs;
