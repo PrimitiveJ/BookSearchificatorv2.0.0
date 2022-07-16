c64d059bafd00e5c258ea29cf0b3974b0440a05d
Primitive J <hokecj@gmail.com>
2022-07-16 13:00:03 -0400

    added different mutations, imported gql from apollo client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,76 @@
+//IMPORTS
+import { gql } from "@apollo/client";
+
+//LOGIN MUTATION
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//ADD USER MUTATION
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
+
+//SAVE BOOK MUTATION
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook: savedBook!) {
+    saveBook(newBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+//REMOVE BOOK MUTATION
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
